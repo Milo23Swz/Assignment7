b81b8f5d5157f4bf6d1899dfbfc30dfed834205d
Emilio Swartz <emiliojc23@gmail.com>
2020-11-19 16:43:59 +0200

    first commit

---
+++ Assignment7app/Assignment7app/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment7app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new TabbedPage1());
+            navPage.BarBackgroundColor = Color.FromHex("#000000");
+
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
